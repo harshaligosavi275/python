377f142cb198cfa325f1ef059f366ba803ba9190
Harshali Gosavi <147470285+harshaligosavi275@users.noreply.github.com>
2024-05-20 17:02:10 +0530

    Create databaseCoffeeMachine.py
    database for coffee machine

---
+++ databaseCoffeeMachine.py
@@ -0,0 +1,31 @@
+
+menu = {
+   "cappuccino": {
+       "Ingredients":{
+                      "water":250,
+                      "milk":100,
+                      "coffee":24
+                     },
+        "cost":200
+
+    },
+   
+   "espresso": {
+        "Ingredients":{
+                       "water":150,
+                        "milk":80,
+                       "coffee":25,
+                    },
+       "cost":100
+    },
+   
+    "latte":{
+       "Ingredients":{
+                      "water":250,
+                      "milk":150,
+                      "coffee":24,
+                    },
+       "cost":150
+    }
+    
+}
